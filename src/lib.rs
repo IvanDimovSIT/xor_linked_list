a9e1a80fa02664144301b43253d2cf6828978bc8
IvanDimovSIT <ivan390@abv.bg>
2025-12-12 16:45:16 +0200

    Implemented standard traits for XorLinkedList

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-use std::ptr::null_mut;
+use std::{
+    fmt::Debug,
+    ops::{Index, IndexMut},
+    ptr::null_mut,
+};
+
+const INDEX_BOUNDS_ERROR: &str = "Index is out of bounds";
 
 #[inline(always)]
 fn xor_next_ptr<T>(
@@ -28,6 +34,90 @@ impl<T> XorLinkedList<T> {
             start: null_mut(),
             end: null_mut(),
         }
+    }
+
+    pub fn peek_front(&self) -> Option<&T> {
+        if self.size == 0 {
+            None
+        } else {
+            unsafe { Some(&(*self.start).payload) }
+        }
+    }
+
+    pub fn peek_front_mut(&self) -> Option<&mut T> {
+        if self.size == 0 {
+            None
+        } else {
+            unsafe { Some(&mut (*self.start).payload) }
+        }
+    }
+
+    pub fn peek_back(&self) -> Option<&T> {
+        if self.size == 0 {
+            None
+        } else {
+            unsafe { Some(&(*self.end).payload) }
+        }
+    }
+
+    pub fn peek_back_mut(&self) -> Option<&mut T> {
+        if self.size == 0 {
+            None
+        } else {
+            unsafe { Some(&mut (*self.end).payload) }
+        }
+    }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        if index >= self.size {
+            return None;
+        }
+
+        let mut prev_ptr = null_mut();
+        let (mut current_ptr, mut jump_count) = if index > self.size / 2 {
+            (self.end, self.size - index - 1)
+        } else {
+            (self.start, index)
+        };
+        while jump_count > 0 {
+            let new_ptr;
+            unsafe {
+                new_ptr = xor_next_ptr((*current_ptr).xor_ptr, prev_ptr);
+            }
+            prev_ptr = current_ptr;
+            current_ptr = new_ptr;
+            jump_count -= 1;
+        }
+
+        Some(unsafe { &(*current_ptr).payload })
+    }
+
+    pub fn get_mut(&mut self, index: usize) -> Option<&mut T> {
+        if index >= self.size {
+            return None;
+        }
+
+        let mut prev_ptr = null_mut();
+        let (mut current_ptr, mut jump_count) = if index > self.size / 2 {
+            (self.end, self.size - index - 1)
+        } else {
+            (self.start, index)
+        };
+        while jump_count > 0 {
+            let new_ptr;
+            unsafe {
+                new_ptr = xor_next_ptr((*current_ptr).xor_ptr, prev_ptr);
+            }
+            prev_ptr = current_ptr;
+            current_ptr = new_ptr;
+            jump_count -= 1;
+        }
+
+        Some(unsafe { &mut (*current_ptr).payload })
+    }
+
+    pub fn len(&self) -> usize {
+        self.size
     }
 
     unsafe fn push_end(end_ptr1: &mut *mut XorNode<T>, end_ptr2: &mut *mut XorNode<T>, value: T) {
@@ -83,14 +173,14 @@ impl<T> XorLinkedList<T> {
 
     pub fn pop_front(&mut self) -> Option<T> {
         unsafe {
-            self.size = self.size.wrapping_sub(1);
+            self.size = self.size.saturating_sub(1);
             Self::pop_end(&mut self.start, &mut self.end)
         }
     }
 
     pub fn pop_back(&mut self) -> Option<T> {
         unsafe {
-            self.size = self.size.wrapping_sub(1);
+            self.size = self.size.saturating_sub(1);
             Self::pop_end(&mut self.end, &mut self.start)
         }
     }
@@ -99,6 +189,44 @@ impl<T> XorLinkedList<T> {
         ReverseXorLinkedListIter {
             xor_linked_list: self,
         }
+    }
+}
+impl<T> Extend<T> for XorLinkedList<T> {
+    fn extend<A: IntoIterator<Item = T>>(&mut self, iter: A) {
+        for element in iter {
+            self.push_back(element);
+        }
+    }
+}
+impl<T> Index<usize> for XorLinkedList<T> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        self.get(index).expect(INDEX_BOUNDS_ERROR)
+    }
+}
+impl<T> IndexMut<usize> for XorLinkedList<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        self.get_mut(index).expect(INDEX_BOUNDS_ERROR)
+    }
+}
+impl<T: Clone> Clone for XorLinkedList<T> {
+    fn clone(&self) -> Self {
+        let mut cloned_list = XorLinkedList::new();
+        for element in self {
+            cloned_list.push_back(element.clone());
+        }
+        cloned_list
+    }
+}
+impl<T> Default for XorLinkedList<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+impl<T: Debug> Debug for XorLinkedList<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_list().entries(self.into_iter()).finish()
     }
 }
 impl<T> Drop for XorLinkedList<T> {
@@ -121,6 +249,36 @@ impl<T> IntoIterator for XorLinkedList<T> {
         }
     }
 }
+impl<'a, T> IntoIterator for &'a XorLinkedList<T> {
+    type Item = &'a T;
+    type IntoIter = RefXorLinkedListIter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let current_ptr = self.start;
+        let prev_ptr = null_mut();
+
+        RefXorLinkedListIter {
+            _xor_linked_list: self,
+            current_ptr,
+            prev_ptr,
+        }
+    }
+}
+impl<'a, T> IntoIterator for &'a mut XorLinkedList<T> {
+    type Item = &'a mut T;
+    type IntoIter = MutRefXorLinkedListIter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let current_ptr = self.start;
+        let prev_ptr = null_mut();
+
+        MutRefXorLinkedListIter {
+            _xor_linked_list: self,
+            current_ptr,
+            prev_ptr,
+        }
+    }
+}
 
 pub struct XorLinkedListIter<T> {
     xor_linked_list: XorLinkedList<T>,
@@ -130,6 +288,52 @@ impl<T> Iterator for XorLinkedListIter<T> {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.xor_linked_list.pop_front()
+    }
+}
+
+pub struct RefXorLinkedListIter<'a, T> {
+    _xor_linked_list: &'a XorLinkedList<T>,
+    current_ptr: *mut XorNode<T>,
+    prev_ptr: *mut XorNode<T>,
+}
+impl<'a, T> Iterator for RefXorLinkedListIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_ptr.is_null() {
+            return None;
+        }
+        unsafe {
+            let payload_ref = &(*self.current_ptr).payload;
+            let new_ptr = xor_next_ptr((*self.current_ptr).xor_ptr, self.prev_ptr);
+            self.prev_ptr = self.current_ptr;
+            self.current_ptr = new_ptr;
+
+            Some(payload_ref)
+        }
+    }
+}
+
+pub struct MutRefXorLinkedListIter<'a, T> {
+    _xor_linked_list: &'a mut XorLinkedList<T>,
+    current_ptr: *mut XorNode<T>,
+    prev_ptr: *mut XorNode<T>,
+}
+impl<'a, T> Iterator for MutRefXorLinkedListIter<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_ptr.is_null() {
+            return None;
+        }
+        unsafe {
+            let payload_ref = &mut (*self.current_ptr).payload;
+            let new_ptr = xor_next_ptr((*self.current_ptr).xor_ptr, self.prev_ptr);
+            self.prev_ptr = self.current_ptr;
+            self.current_ptr = new_ptr;
+
+            Some(payload_ref)
+        }
     }
 }
 
@@ -149,16 +353,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_memory() {
-        for _ in 0..10000 {
-            let mut list: XorLinkedList<u128> = XorLinkedList::new();
-            for i in 0..10000u128 {
-                list.push_front(i);
-            }
-        }
-    }
-
-    #[test]
     fn test_add_iterate() {
         let mut list: XorLinkedList<i32> = XorLinkedList::new();
         list.push_front(2);
@@ -176,6 +370,124 @@ mod tests {
     }
 
     #[test]
+    fn test_iterate_ref() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+
+        let mut items: Vec<i32> = vec![];
+        for i in &list {
+            items.push(*i);
+        }
+
+        assert_eq!(1, items[0]);
+        assert_eq!(2, items[1]);
+        assert_eq!(3, items[2]);
+    }
+
+    #[test]
+    fn test_iterate_mut_ref() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+
+        for i in &mut list {
+            *i += 100;
+        }
+
+        let mut items: Vec<i32> = vec![];
+        for i in &list {
+            items.push(*i);
+        }
+
+        assert_eq!(101, items[0]);
+        assert_eq!(102, items[1]);
+        assert_eq!(103, items[2]);
+    }
+
+    #[test]
+    fn test_get() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+
+        assert_eq!(1, *list.get(0).unwrap());
+        assert_eq!(2, *list.get(1).unwrap());
+        assert_eq!(3, *list.get(2).unwrap());
+        assert!(list.get(100).is_none());
+    }
+
+    #[test]
+    fn test_get_mut() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+
+        *list.get_mut(0).unwrap() += 100;
+        *list.get_mut(1).unwrap() += 100;
+        *list.get_mut(2).unwrap() += 100;
+
+        assert_eq!(101, *list.get(0).unwrap());
+        assert_eq!(102, *list.get(1).unwrap());
+        assert_eq!(103, *list.get(2).unwrap());
+        assert!(list.get_mut(100).is_none());
+    }
+
+    #[test]
+    fn test_index() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+
+        assert_eq!(1, list[0]);
+        assert_eq!(2, list[1]);
+        assert_eq!(3, list[2]);
+    }
+
+    #[test]
+    fn test_index_mut() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+
+        list[0] += 100;
+        list[1] += 100;
+        list[2] += 100;
+
+        assert_eq!(101, list[0]);
+        assert_eq!(102, list[1]);
+        assert_eq!(103, list[2]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_index_out_of_bounds() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+
+        list[100];
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_mut_index_out_of_bounds() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+
+        list[100] += 100;
+    }
+
+    #[test]
     fn test_add_reverse_iterate() {
         let mut list: XorLinkedList<i32> = XorLinkedList::new();
         list.push_front(2);
@@ -190,5 +502,66 @@ mod tests {
         assert_eq!(3, items[0]);
         assert_eq!(2, items[1]);
         assert_eq!(1, items[2]);
+    }
+
+    #[test]
+    fn test_peek_front() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        assert!(list.peek_front().is_none());
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+
+        assert_eq!(1, *list.peek_front().unwrap());
+    }
+
+    #[test]
+    fn test_peek_back() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        assert!(list.peek_back().is_none());
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+
+        assert_eq!(3, *list.peek_back().unwrap());
+    }
+
+    #[test]
+    fn test_peek_front_mut() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        assert!(list.peek_front_mut().is_none());
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+        *list.peek_front_mut().unwrap() += 100;
+
+        assert_eq!(101, *list.peek_front().unwrap());
+    }
+
+    #[test]
+    fn test_peek_back_mut() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        assert!(list.peek_back_mut().is_none());
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+        *list.peek_back_mut().unwrap() += 100;
+
+        assert_eq!(103, *list.peek_back().unwrap());
+    }
+
+    #[test]
+    fn test_clone() {
+        let mut list: XorLinkedList<i32> = XorLinkedList::new();
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+
+        let cloned_list = list.clone();
+        assert_eq!(3, cloned_list.len());
+
+        for (i, j) in list.into_iter().zip(cloned_list.into_iter()) {
+            assert_eq!(i, j);
+        }
     }
 }
